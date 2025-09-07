9f0e8515e3987a36a0ed32fe61248e73436cc9b5
turnrua <79034334+turnrua@users.noreply.github.com>
2025-09-07 15:11:42 +0800

    Add files via upload

---
+++ pvpsidou.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+using System.Numerics;
+using Newtonsoft.Json;
+using Dalamud.Utility.Numerics;
+using KodakkuAssist.Script;
+using KodakkuAssist.Module.GameEvent;
+using KodakkuAssist.Module.Draw;
+
+namespace PVP战士死斗提示;
+
+[ScriptType(guid: "5EB4C3B1-7310-4964-B269-6569AE4F43D7", name: "PVP战士死斗提示", territorys: [], version: "0.0.0.2", author: "chris")]
+public class 机工LBClass
+{
+    [ScriptMethod(name: "战士死斗提示", eventType: EventTypeEnum.ActionEffect, eventCondition: ["ActionId:29081"])]
+    public void 机工LB(Event @event, ScriptAccessory accessory)
+    {
+        if (@event.TargetId() != accessory.Data.Me) return;
+
+        accessory.Method.TextInfo("死斗!", 2000);
+    }
+}
+
+
+public static class EventExtensions
+{
+    private static bool ParseHexId(string? idStr, out uint id)
+    {
+        id = 0;
+        if (string.IsNullOrEmpty(idStr)) return false;
+        try
+        {
+            var idStr2 = idStr.Replace("0x", "");
+            id = uint.Parse(idStr2, System.Globalization.NumberStyles.HexNumber);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public static uint TargetId(this Event @event)
+    {
+        return ParseHexId(@event["TargetId"], out var id) ? id : 0;
+    }
+}
