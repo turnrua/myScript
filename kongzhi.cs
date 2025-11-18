d7a51888e636f15ccee70ef84dfed9e3be566b34
turnrua <79034334+turnrua@users.noreply.github.com>
2025-11-18 15:16:52 +0800

    Update kongzhi.cs

--- kongzhi.cs
+++ kongzhi.cs
@@ -62,7 +62,7 @@ public class 机工LBClass
         if (isEdgeTTS) accessory.Method.EdgeTTS("净化净化");
     }
      [ScriptMethod(name: "战场变猪", eventType: EventTypeEnum.StatusAdd, eventCondition: ["StatusID:3085"])]
-    public void 战场变猪(Event @event, ScriptAccessory accessory)
+    public async void 战场变猪(Event @event, ScriptAccessory accessory)
     {
         if (@event.SourceId() != accessory.Data.Me) return;
         await Task.Delay(100);
